67acc55c4fa4a493d0f944aaed775c2a234e2b11
Bhavleen <singhbhavleen3@gmail.com>
2021-01-19 21:44:35 +0530

    Dict in Python

---
+++ sortDict.py
@@ -0,0 +1,4 @@
+# Write a Python program to sort (ascending and descending) a dictionary by value. [use sorted()]
+dic = {1: 'World', 2: 'Hello', 3: 'My name', 4: 'Anyone'}
+dicS = sorted(dic.values())
+print(dicS)
