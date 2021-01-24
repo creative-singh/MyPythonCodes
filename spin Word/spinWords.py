a6d0b321c50462cd546409b6c56edc99dd5d735b
Bhavleen <singhbhavleen3@gmail.com>
2021-01-24 21:12:15 +0530

    Spining words

---
+++ spin Word/spinWords.py
@@ -0,0 +1,17 @@
+# def spinWords(string):
+#     res=""
+#     for i in string:
+#         if len(i) >=5:
+#           res=i[::-1]
+#         res=i
+
+
+# print(spinWords("Hey fellow warriors"))
+# print(spinWords("This is a test"))
+# print(spinWords("This is another test"))
+
+
+string = "hello my name is lovely"
+
+for i in string:
+    print(i)
