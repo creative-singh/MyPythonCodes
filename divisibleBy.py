f233dbbfdd3376312fd79f0cdb5ff348ed05b7b0
Bhavleen <singhbhavleen3@gmail.com>
2021-01-29 18:35:09 +0530

    Divisible

---
+++ divisibleBy.py
@@ -0,0 +1,10 @@
+#Challenge - Using range and for loop, print all multiples of 5, 7, 11 from 1 to 1001
+
+for i in range(1, 1001):
+    if i % 5 == 0 :
+        print(f"Divisible by {i} ")
+    if i % 7 == 0 :
+        print(f"Divisible by {i} ")
+    if i % 11 == 0 :
+        print(f"Divisible by {i} ")
+    
